9a22f711d913ef0fa424401a7a3db52fd2212f7d
“maki5588” <your-mail@example.com>
2020-10-20 23:25:43 +0900

    update: 計算処理

--- src/constructor.rb
+++ src/constructor.rb
@@ -3,32 +3,24 @@ require 'active_support'
 require 'active_support/core_ext'
 
 class Constructor
-	def self.output(material_hash, recipe_hash)
-	recipe_hash.each do |recipe|
-		target_name = recipe["材料名"]
-		target_quantity = recipe["2人前の使用量"]
-		# puts target
-		# puts material_hash.select {|k, v| v == target }
-		material_hash.each do |material|
-			puts material_target_name = material.detect {|k, v| v == target_name }
-			puts material_target_quantity = material.detect {|k, v| v == target_quantity }
-			# puts target_name
-			# puts target_name
-			# puts material["100gあたりのカロリー"]
-			# puts material["100gあたりの食塩相当量"]
-			# puts material["100gあたりの食塩相当量"]
-		end
-		# material_hash.each do |material|
-		#   # puts matersial
-		#   pumaterial_name = material.detect {|k, v| v == target_name }
-		#   puts material.detect {|k, v| v == target_quantity }
-		# end
-	end
-	# target = material_hash[0]["材料名"]
-	# targeted = recipe_json.["#{target}"]
-	# p "recipe"
-	# puts targeted
-	end
+  def self.output(material_hash, recipe_hash)
+  	recipe_hash.each do |recipe|
+  	  puts recipe
+      target_name = recipe["材料名"]
+      target_quantity = recipe["2人前の使用量"]
+      # puts target
+      # puts material_hash.select {|k, v| v == target }
+      material_hash.each do |material|
+        # puts matersial
+        pumaterial_name = material.detect {|k, v| v == target_name }
+        puts material.detect {|k, v| v == target_quantity }
+      end
+    end
+    # target = material_hash[0]["材料名"]
+    # targeted = recipe_json.["#{target}"]
+    # p "recipe"
+    # puts targeted
+  end
 end
 
 Constructor.output(Extraction.material_json, Extraction.recipe_json)
--- src/insert.rb
+++ src/insert.rb
@@ -1,5 +1,7 @@
 require './extraction.rb'
 require "csv"
+# require "mathn"
+# include Math
 
 class Insert
   def self.merged_data_write_to_csv(material_hash, recipe_hash)
@@ -20,12 +22,32 @@ class Insert
   def self.to_g
     puts "start..."
     csv_data = CSV.foreach('./../data/sample.csv', headers: false) do |data|
-      unless data[4].nil?
-        
-      end
+      # unless data[4].empty?
+        if data[3].include?("パック")
+          num = data[3].sub("パック", "").to_i
+          regular = data[4].sub(/1パックあたり/, '').sub(/g/, '')
+          puts portion = regular * num
+        elsif data[3].include?("少々")
+          puts portion = data[4].sub('少々は', '').sub(/gとする/, '')
+        elsif data[3].include?("大さじ")
+          num = data[3].sub("大さじ", "").to_i
+          regular = data[4].sub(/大さじ1あたり/, '').sub(/g/, '')
+          puts portion = regular * num
+        elsif data[3].include?("小さじ1")
+        # arrey
+          # puts num = data[3].gsub(/[^\d+\/]/, "").sub("/", ',').split(/,/, 1)
+          # puts num_eval = eval("{#{num}}")
+        # not arrey
+          num = data[3].gsub(/[^\d+\/]/, "")
+          num_w = Rational(num).to_f
+          regular = data[4].sub(/小さじ1あたり/, '').sub(/g/, '').to_i
+          puts portion = regular * num_w
+        end
+      # end
     end
   end
-
+  def pack
+  end
 end
 
 Insert.merged_data_write_to_csv(Extraction.material_hash, Extraction.recipe_hash)
