4a1afbfecdbf33c8643a49a5c7dc0a98bc2e17e4
“maki5588” <your-mail@example.com>
2020-10-21 18:14:34 +0900

    add: output.rb

---
+++ src/output.rb
@@ -0,0 +1,14 @@
+require_relative "insert.rb"
+
+class Output
+  def self.output(sodium_intake_sum, calorie_intake_sum)
+    puts "2人前のカロリーは#{calorie_intake_sum} kcalで塩分は#{sodium_intake_sum} gです"
+  end
+end
+
+
+# Insert.merged_data_write_to_csv(Input.material_hash, Input.recipe_hash)
+# Insert.unit_normalization
+# sodium_sum = Insert.sodium_intakes_sum(Input.material_hash)
+# calorie_sum = Insert.calorie_intakes_sum(Input.material_hash)
+# Output.output(sodium_sum, calorie_sum)
