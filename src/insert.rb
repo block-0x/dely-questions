9c2690f04eaa3f664dfe9cc18671ec471457a842
“maki5588” <your-mail@example.com>
2020-10-22 04:43:25 +0900

    clean

--- src/insert.rb
+++ src/insert.rb
@@ -16,41 +16,66 @@ class Normalization
     end
   end
 
-  def unit_normalization()
+  def unit_normalization_module()
   	@portions = []
-    @merged_data.each do |data|
-      amount_to_use = data["2人前の使用量"]
-      material_note = data["備考"]
-      unless material_note.nil?
-        aaa(amount_to_use, material_note)
-        # if amount_to_use.include?("パック")
-        #   num = amount_to_use.sub("パック", "").to_f
-        #   regular = material_note.sub(/1パックあたり/, '').sub(/g/, '')
-        #   @portion = (regular * num).to_f
-        if amount_to_use.include?("少々")
-          @portion = (material_note.sub('少々は', '').sub(/gとする/, '')).to_f
-        elsif amount_to_use.include?("大さじ")
-          num = amount_to_use.sub("大さじ", "").to_f
-          regular = material_note.sub(/大さじ1あたり/, '').sub(/g/, '')
-          @portion = (regular * num).to_f
-        elsif amount_to_use.include?("小さじ1")
-          num = amount_to_use.gsub(/[^\d+\/]/, "")
-          num_w = Rational(num).to_f
-          regular = material_note.sub(/小さじ1あたり/, '').sub(/g/, '').to_f
-          @portion = regular * num_w
-        end
+    @merged_data.each do |merged_data|
+      @two_use = merged_data["2人前の使用量"]
+      @note = merged_data["備考"]
+      unless @note.nil?
+        if pack || pinch || l_spoon || s_spoon ;end
       else
-        @portion = amount_to_use.sub(/g/, '').to_f
+        @portion = @two_use.sub(/g/, '').to_f
       end
       @portions.push(@portion)
     end
   end
 
-  def aaa(amount_to_use, material_note)
-    if amount_to_use.include?("パック")
-      num = amount_to_use.sub("パック", "").to_f
-      regular = material_note.sub(/1パックあたり/, '').sub(/g/, '')
-      return @portion = (regular * num).to_f
+  def pack()
+    about = "パック"
+    if @two_use.include?(about)
+      integer_or_rational?
+    end
+  end
+
+  def integer_or_rational?()
+    @about_amount = @two_use.gsub(/[^\d+\/]/, "")
+    @normalization_num = @note.match(/あたり(.*)/).to_s.gsub(/[^\d+\/]/, "").to_f
+    if rational?()
+    else
+      @portion = (@normalization_num * @about_amount.to_f)
+    end
+  end
+
+  def pinch()
+    about = "少々"
+    if @two_use.include?(about)
+      @portion = (@note.sub('少々は', '').sub(/gとする/, '')).to_f
+    end
+  end
+
+  def l_spoon()
+    about = "大さじ"
+    if @two_use.include?(about)
+      about_amount = @two_use.gsub(/[^\d+\/]/, "")
+      about_amount = @two_use.sub("大さじ", "").to_f
+      normalization_num = @note.sub(/大さじ1あたり/, '').sub(/g/, '').to_f
+      @portion = (normalization_num * about_amount)
+    end
+  end
+
+  def s_spoon()
+    if @two_use.include?("小さじ1")
+      about_amount = @two_use.gsub(/[^\d+\/]/, "")
+      about_amount_to_float = Rational(about_amount).to_f
+      normalization_num = @note.sub(/小さじ1あたり/, '').sub(/g/, '').to_f
+      @portion = normalization_num * about_amount_to_float
+    end
+  end
+
+  def rational?()
+    if @about_amount.include?("/")
+      @about_amount_to_float = Rational(@about_amount).to_f
+      @portion = (@normalization_num * @about_amount_to_float)
     end
   end
 
@@ -78,7 +103,7 @@ end
 if __FILE__ == $0
   normalization = Normalization.new(Input.material_data, Input.recipe_data)
   normalization.material_data_recipe_data_merged()
-  normalization.unit_normalization()
+  normalization.unit_normalization_module()
   sodium_sum = normalization.sodium_intakes_sum()
   calorie_sum = normalization.calorie_intakes_sum()
 end
--- src/run.rb
+++ src/run.rb
@@ -7,7 +7,7 @@ require_relative "output.rb"
 if __FILE__ == $0
   normalization = Normalization.new(Input.material_data, Input.recipe_data)
   normalization.material_data_recipe_data_merged()
-  normalization.unit_normalization()
+  normalization.unit_normalization_module()
   sodium_sum = normalization.sodium_intakes_sum()
   calorie_sum = normalization.calorie_intakes_sum()
   Output.output(sodium_sum, calorie_sum)
